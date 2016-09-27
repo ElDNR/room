cf191e14bc4edaaf022ecd28a402d3466c575069
DnR <diegonunezr@gmail.com>
2016-09-27 17:53:24 -0400

    Created default VS Web Application project to serve as the server component of the room application.

---
+++ src/room.server.web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace room.server.web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/room.server.web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace room.server.web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
