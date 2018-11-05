3f986f7fcc5c3585c3a8e0da0e74e7ae0008a6a7
DnR <diegonunezr@gmail.com>
2018-11-05 14:32:57 -0500

    Registering the new dashboard routes with the room module.

--- src/room.server.web/ClientApp/src/app/room/room-routing.module.ts
+++ src/room.server.web/ClientApp/src/app/room/room-routing.module.ts
@@ -3,10 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RoomLoginComponent } from './login/room.login.component';
 import { RoomDashboardComponent } from './dashboard/room.dashboard.component';
+import { RoomDashboardRenderingsComponent } from './dashboard/renderings/room.dashboard.renderings.component';
+import { RoomDashboardRenderingsEditComponent } from './dashboard/renderings/room.dashboard.renderings.edit.component';
+import { RoomDashboardRenderingTypesComponent } from './dashboard/renderingtypes/room.dashboard.renderingtypes.component';
+import { RoomDashboardRenderingTypesEditComponent } from './dashboard/renderingtypes/room.dashboard.renderingtypes.edit.component';
+import { RoomDashboardScenesComponent } from './dashboard/scenes/room.dashboard.scenes.component';
+import { RoomDashboardScenesEditComponent } from './dashboard/scenes/room.dashboard.scenes.edit.component';
 
 const roomRoutes: Routes = [
   { path: 'room', component: RoomLoginComponent },
-  { path: 'room/dashboard', component: RoomDashboardComponent }
+  { path: 'room/dashboard', component: RoomDashboardComponent },
+  { path: 'room/dashboard/renderings', component: RoomDashboardRenderingsComponent },
+  { path: 'room/dashboard/renderings/edit/:id', component: RoomDashboardRenderingsEditComponent },
+  { path: 'room/dashboard/renderingtypes', component: RoomDashboardRenderingTypesComponent },
+  { path: 'room/dashboard/renderingtypes/edit/:id', component: RoomDashboardRenderingTypesEditComponent },
+  { path: 'room/dashboard/scenes', component: RoomDashboardScenesComponent },
+  { path: 'room/dashboard/scenes/edit/:id', component: RoomDashboardScenesEditComponent }
 ];
 
 @NgModule({
--- src/room.server.web/ClientApp/src/app/room/room.module.ts
+++ src/room.server.web/ClientApp/src/app/room/room.module.ts
@@ -4,6 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { RoomLoginComponent } from './login/room.login.component';
 import { RoomDashboardComponent } from './dashboard/room.dashboard.component';
+import { RoomDashboardRenderingsComponent } from './dashboard/renderings/room.dashboard.renderings.component';
+import { RoomDashboardRenderingsEditComponent } from './dashboard/renderings/room.dashboard.renderings.edit.component';
+import { RoomDashboardRenderingTypesComponent } from './dashboard/renderingtypes/room.dashboard.renderingtypes.component';
+import { RoomDashboardRenderingTypesEditComponent } from './dashboard/renderingtypes/room.dashboard.renderingtypes.edit.component';
+import { RoomDashboardScenesComponent } from './dashboard/scenes/room.dashboard.scenes.component';
+import { RoomDashboardScenesEditComponent } from './dashboard/scenes/room.dashboard.scenes.edit.component';
 
 import { RoomRoutingModule } from './room-routing.module';
 
@@ -15,10 +21,14 @@ import { RoomRoutingModule } from './room-routing.module';
   ],
   declarations: [
     RoomLoginComponent,
-    RoomDashboardComponent
+    RoomDashboardComponent,
+    RoomDashboardRenderingsComponent,
+    RoomDashboardRenderingsEditComponent,
+    RoomDashboardRenderingTypesComponent,
+    RoomDashboardRenderingTypesEditComponent,
+    RoomDashboardScenesComponent,
+    RoomDashboardScenesEditComponent
   ]
 })
 export class RoomModule {
-
-  
 }
